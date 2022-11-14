dfd941ae8f15bc8ed64678bc11ac334652f045d5
Shlef <mohamedsaidwork@gmail.com>
2022-11-14 04:30:55 +0200

    Implemented Authuntication page - Sign in page

---
+++ src/components/common/auth/AuthWrapper.tsx
@@ -0,0 +1,72 @@
+import React, { Children } from 'react';
+import { NavLink } from 'react-router-dom';
+
+interface AuthWrapperProps {}
+interface MyProps {
+  children?: React.ReactNode;
+}
+
+export const AuthWrapper: React.FC<AuthWrapperProps> = (props: MyProps) => {
+  return (
+    <>
+      {/* Left decoration */}
+      <img
+        src="/assets/images/auth-decoration-left.png"
+        alt="golden planet"
+        className="left-auth-decoration"
+      />
+      <img
+        src="/assets/images/auth-decoration-left.png"
+        alt="golden planet"
+        className="left-auth-decoration left-auth-decoration-varient "
+      />
+      {/* Nav & Children */}
+      <div className="max-w-screen-xl m-auto">
+        <header className="flex justify-center">
+          <div
+            className="
+      flex justify-center gap-[2rem]
+      sm:gap-[15rem]
+      text-2xl 
+      w-full 
+      border-gray-300 border-solid border-b-[1px]"
+          >
+            <NavLink
+              to="/signup"
+              className={({ isActive }) =>
+                isActive
+                  ? 'active-auth-navlink focus-visible-state'
+                  : 'auth-navlink focus-visible-state hover:text-slate-600 hover:drop-shadow-md'
+              }
+            >
+              Sign up
+            </NavLink>
+            <NavLink
+              to="/signin"
+              className={({ isActive }) =>
+                isActive
+                  ? 'active-auth-navlink focus-visible-state'
+                  : 'auth-navlink focus-visible-state hover:text-slate-600 hover:drop-shadow-md'
+              }
+            >
+              Login
+            </NavLink>
+          </div>
+        </header>
+        {/* Children goes here */}
+        {props.children}
+      </div>
+      {/* Right decoration */}
+      <img
+        src="/assets/images/auth-decoration-right.png"
+        alt="golden planet"
+        className="right-auth-decoration right-auth-decoration-varient"
+      />
+      <img
+        src="/assets/images/auth-decoration-right.png"
+        alt="golden planet"
+        className="right-auth-decoration"
+      />
+    </>
+  );
+};
---
+++ src/pages/Signin.tsx
@@ -0,0 +1,124 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import {
+  Google as GoogleButton,
+  Facebook as FacebookButton,
+} from '@components/';
+
+interface SigninProps {}
+
+export const Signin: React.FC<SigninProps> = ({}) => {
+  return (
+    <>
+      <form
+        action="#!"
+        className="max-w-screen-lg my-9 mx-auto flex flex-col gap-5 px-3"
+      >
+        {/* Email */}
+        <div className="form-group-col">
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className="form-input focus-visible-state"
+          />
+        </div>
+        {/* Password */}
+        <div className="form-group-col">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            className="form-input focus-visible-state"
+          />
+        </div>
+        {/* Remember me + Forget password */}
+        <div className="flex flex-col items-center gap-4 sm:gap-0 text-lg sm:flex-row justify-between">
+          {/* Remember me */}
+          <div className="form-group-row w-max">
+            <input
+              type="checkbox"
+              name="checkbox"
+              id="checkbox"
+              className="focus-visible-state"
+            />
+            <label
+              htmlFor="checkbox"
+              className="
+              form-label text-gray-500 cursor-pointer"
+            >
+              Remember me
+            </label>
+          </div>
+
+          {/* Forget password */}
+          <NavLink
+            to="/password-reset"
+            className="text-primary hover:text-slate-500 underline font-medium text-lg whitespace-nowrap pr-[15px] smooth-transition focus-visible-state"
+          >
+            Forget password?
+          </NavLink>
+        </div>
+
+        {/* Login button */}
+        <button
+          className="bg-primary hover:bg-slate-500
+        smooth-transition text-white py-4 mt-4 rounded-md font-medium focus-visible-state"
+        >
+          Login
+        </button>
+
+        {/* Signup */}
+        <p className="text-gray-500 text-center text-sm sm:text-lg">
+          Don't have an account?{' '}
+          <NavLink
+            to="/signup"
+            className="text-primary font-bold focus-visible-state hover:text-gray-700 smooth-transition"
+          >
+            Sign up
+          </NavLink>
+        </p>
+        {/* Other Sign in methods */}
+        <div className="flex flex-nowrap items-center gap-2 px-9 mb-5">
+          {/* before line */}
+          <svg
+            aria-hidden="true"
+            width="100%"
+            height="2"
+            viewBox="0 0 522 2"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <line x1="0.613281" y1="1" x2="521.613" y2="1" stroke="#9269BA" />
+          </svg>
+          {/* paragraph */}
+          <p className="text-primary text-center relative whitespace-nowrap">
+            or continue with
+          </p>
+          {/* after line */}
+          <svg
+            aria-hidden="true"
+            width="100%"
+            height="2"
+            viewBox="0 0 522 2"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <line x1="0.613281" y1="1" x2="521.613" y2="1" stroke="#9269BA" />
+          </svg>
+        </div>
+        {/* Google & Facebook Signin methods */}
+        <div className="flex flex-col justify-between gap-[1rem] sm:flex-row sm:gap-[4rem]">
+          <GoogleButton />
+          <FacebookButton />
+        </div>
+      </form>
+    </>
+  );
+};
